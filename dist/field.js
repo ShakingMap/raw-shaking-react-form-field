3350ef734da46f7d779de2b43b7d8e1fb1bf167d
zhaoyao91 <zhaoyao91@163.com>
2016-04-27 01:15:04 +0800

    first commit

---
+++ dist/field.js
@@ -0,0 +1,432 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/**
+ * props for fields, all are optional
+ * - {String} label
+ * - {String} type
+ * - {Any} options
+ * - {Any} value
+ * - {Func(value)} onChange
+ * - {String} validationState - 'success' or 'error'
+ * - {String} validationError
+ */
+
+var fieldClass = 'field';
+
+// support types: text, email, password, file
+// support options: placeholder
+
+var InputField = function (_React$Component) {
+    _inherits(InputField, _React$Component);
+
+    function InputField(props) {
+        _classCallCheck(this, InputField);
+
+        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(InputField).call(this, props));
+
+        _this.id = Math.random() + '';
+        return _this;
+    }
+
+    _createClass(InputField, [{
+        key: 'render',
+        value: function render() {
+            var _props = this.props;
+            var label = _props.label;
+            var type = _props.type;
+            var options = _props.options;
+            var _onChange = _props.onChange;
+            var value = _props.value;
+            var validationState = _props.validationState;
+            var validationError = _props.validationError;
+
+            var _ref = options || {};
+
+            var placeholder = _ref.placeholder;
+
+
+            var validationClass = validationState ? 'validation-' + validationState : '';
+            var shouldShowError = validationState === 'error' && validationError;
+
+            return _react2.default.createElement(
+                'div',
+                { className: fieldClass + ' ' + validationClass },
+                label ? _react2.default.createElement(
+                    'label',
+                    { htmlFor: this.id },
+                    label
+                ) : null,
+                _react2.default.createElement('input', {
+                    id: this.id,
+                    type: type,
+                    placeholder: placeholder,
+                    value: value || '',
+                    onChange: function onChange(e) {
+                        return _onChange(e.target.value);
+                    }
+                }),
+                shouldShowError ? _react2.default.createElement(
+                    'div',
+                    { className: 'error' },
+                    validationError
+                ) : null
+            );
+        }
+    }]);
+
+    return InputField;
+}(_react2.default.Component);
+
+// support types: textarea
+// support options: placeholder, rows
+
+
+var TextareaField = function (_React$Component2) {
+    _inherits(TextareaField, _React$Component2);
+
+    function TextareaField(props) {
+        _classCallCheck(this, TextareaField);
+
+        var _this2 = _possibleConstructorReturn(this, Object.getPrototypeOf(TextareaField).call(this, props));
+
+        _this2.id = Math.random() + '';
+        return _this2;
+    }
+
+    _createClass(TextareaField, [{
+        key: 'render',
+        value: function render() {
+            var _props2 = this.props;
+            var label = _props2.label;
+            var options = _props2.options;
+            var _onChange2 = _props2.onChange;
+            var value = _props2.value;
+            var validationState = _props2.validationState;
+            var validationError = _props2.validationError;
+
+            var _ref2 = options || {};
+
+            var placeholder = _ref2.placeholder;
+            var rows = _ref2.rows;
+
+
+            var validationClass = validationState ? 'validation-' + validationState : '';
+            var shouldShowError = validationState === 'error' && validationError;
+
+            return _react2.default.createElement(
+                'div',
+                { className: fieldClass + ' ' + validationClass },
+                label ? _react2.default.createElement(
+                    'label',
+                    { htmlFor: this.id },
+                    label
+                ) : null,
+                _react2.default.createElement('textarea', {
+                    id: this.id,
+                    placeholder: placeholder,
+                    rows: rows,
+                    value: value || '',
+                    onChange: function onChange(e) {
+                        return _onChange2(e.target.value);
+                    }
+                }),
+                shouldShowError ? _react2.default.createElement(
+                    'div',
+                    { className: 'error' },
+                    validationError
+                ) : null
+            );
+        }
+    }]);
+
+    return TextareaField;
+}(_react2.default.Component);
+
+// support types: group.checkbox
+// support options: group, vertical
+// options.group is an object, where key is checkbox key, and value is checkbox label
+// value is an array, where items are the keys of checked checkboxes
+
+
+var CheckboxGroupField = function (_React$Component3) {
+    _inherits(CheckboxGroupField, _React$Component3);
+
+    function CheckboxGroupField(props) {
+        _classCallCheck(this, CheckboxGroupField);
+
+        var _this3 = _possibleConstructorReturn(this, Object.getPrototypeOf(CheckboxGroupField).call(this, props));
+
+        _this3.id = Math.random() + '';
+        return _this3;
+    }
+
+    _createClass(CheckboxGroupField, [{
+        key: 'render',
+        value: function render() {
+            var _this4 = this;
+
+            var _props3 = this.props;
+            var label = _props3.label;
+            var options = _props3.options;
+            var _onChange3 = _props3.onChange;
+            var value = _props3.value;
+            var validationState = _props3.validationState;
+            var validationError = _props3.validationError;
+
+            var _ref3 = options || {};
+
+            var group = _ref3.group;
+            var vertical = _ref3.vertical;
+
+
+            var validationClass = validationState ? 'validation-' + validationState : '';
+            var shouldShowError = validationState === 'error' && validationError;
+
+            var checkedItems = new Set(value);
+
+            return _react2.default.createElement(
+                'div',
+                { className: fieldClass + ' ' + validationClass },
+                label ? _react2.default.createElement(
+                    'label',
+                    { htmlFor: this.id },
+                    label
+                ) : null,
+                Object.keys(group).map(function (key) {
+                    return _react2.default.createElement(
+                        'div',
+                        {
+                            key: key,
+                            className: 'item',
+                            style: vertical ? {} : { display: 'inline', marginRight: '1rem' }
+                        },
+                        _react2.default.createElement('input', {
+                            id: _this4.id + key,
+                            type: 'checkbox',
+                            checked: checkedItems.has(key),
+                            onChange: function onChange(e) {
+                                var newValue = (value || []).filter(function (x) {
+                                    return x !== key;
+                                });
+                                if (e.target.checked) newValue.push(key);
+                                _onChange3(newValue);
+                            }
+                        }),
+                        _react2.default.createElement(
+                            'label',
+                            { htmlFor: _this4.id + key },
+                            group[key]
+                        )
+                    );
+                }),
+                shouldShowError ? _react2.default.createElement(
+                    'div',
+                    { className: 'error' },
+                    validationError
+                ) : null
+            );
+        }
+    }]);
+
+    return CheckboxGroupField;
+}(_react2.default.Component);
+
+// support types: group.radio
+// support options: group, vertical
+// options.group is an object, where key is radio key, and value is radio label
+// value is the key of selected radio
+
+
+var RadioGroupField = function (_React$Component4) {
+    _inherits(RadioGroupField, _React$Component4);
+
+    function RadioGroupField(props) {
+        _classCallCheck(this, RadioGroupField);
+
+        var _this5 = _possibleConstructorReturn(this, Object.getPrototypeOf(RadioGroupField).call(this, props));
+
+        _this5.id = Math.random() + '';
+        return _this5;
+    }
+
+    _createClass(RadioGroupField, [{
+        key: 'render',
+        value: function render() {
+            var _this6 = this;
+
+            var _props4 = this.props;
+            var label = _props4.label;
+            var options = _props4.options;
+            var _onChange4 = _props4.onChange;
+            var value = _props4.value;
+            var validationState = _props4.validationState;
+            var validationError = _props4.validationError;
+
+            var _ref4 = options || {};
+
+            var group = _ref4.group;
+            var vertical = _ref4.vertical;
+
+
+            var validationClass = validationState ? 'validation-' + validationState : '';
+            var shouldShowError = validationState === 'error' && validationError;
+
+            return _react2.default.createElement(
+                'div',
+                { className: fieldClass + ' ' + validationClass },
+                label ? _react2.default.createElement(
+                    'label',
+                    { htmlFor: this.id },
+                    label
+                ) : null,
+                Object.keys(group).map(function (key) {
+                    return _react2.default.createElement(
+                        'div',
+                        {
+                            key: key,
+                            className: 'item',
+                            style: vertical ? {} : { display: 'inline', marginRight: '1rem' }
+                        },
+                        _react2.default.createElement('input', {
+                            id: _this6.id + key,
+                            type: 'radio',
+                            checked: key === value,
+                            onChange: function onChange(e) {
+                                _onChange4(key);
+                            }
+                        }),
+                        _react2.default.createElement(
+                            'label',
+                            { htmlFor: _this6.id + key },
+                            group[key]
+                        )
+                    );
+                }),
+                shouldShowError ? _react2.default.createElement(
+                    'div',
+                    { className: 'error' },
+                    validationError
+                ) : null
+            );
+        }
+    }]);
+
+    return RadioGroupField;
+}(_react2.default.Component);
+
+// support types: group.select
+// support options: group, multiple
+// options.group is an object, where key is option key, and value is option text
+// if not multiple, value is the key of selected option
+// else value is an array of keys of selected options
+
+
+var SelectGroupField = function (_React$Component5) {
+    _inherits(SelectGroupField, _React$Component5);
+
+    function SelectGroupField(props) {
+        _classCallCheck(this, SelectGroupField);
+
+        var _this7 = _possibleConstructorReturn(this, Object.getPrototypeOf(SelectGroupField).call(this, props));
+
+        _this7.id = Math.random() + '';
+        return _this7;
+    }
+
+    _createClass(SelectGroupField, [{
+        key: 'render',
+        value: function render() {
+            var _props5 = this.props;
+            var label = _props5.label;
+            var options = _props5.options;
+            var onChange = _props5.onChange;
+            var value = _props5.value;
+            var validationState = _props5.validationState;
+            var validationError = _props5.validationError;
+
+            var _ref5 = options || {};
+
+            var group = _ref5.group;
+            var multiple = _ref5.multiple;
+
+
+            var validationClass = validationState ? 'validation-' + validationState : '';
+            var shouldShowError = validationState === 'error' && validationError;
+
+            var onFieldChange = multiple ? function (e) {
+                var options = e.target.options;
+                var value = [];
+                for (var i in options) {
+                    if (options.hasOwnProperty(i) && options[i].selected) value.push(options[i].value);
+                }
+                onChange(value);
+            } : function (e) {
+                return onChange(e.target.value);
+            };
+
+            return _react2.default.createElement(
+                'div',
+                { className: fieldClass + ' ' + validationClass },
+                label ? _react2.default.createElement(
+                    'label',
+                    { htmlFor: this.id },
+                    label
+                ) : null,
+                _react2.default.createElement(
+                    'select',
+                    {
+                        id: this.id,
+                        value: value || (multiple ? [] : ''),
+                        onChange: onFieldChange,
+                        multiple: multiple
+                    },
+                    Object.keys(group).map(function (key) {
+                        return _react2.default.createElement(
+                            'option',
+                            { key: key, value: key },
+                            group[key]
+                        );
+                    })
+                ),
+                shouldShowError ? _react2.default.createElement(
+                    'div',
+                    { className: 'error' },
+                    validationError
+                ) : null
+            );
+        }
+    }]);
+
+    return SelectGroupField;
+}(_react2.default.Component);
+
+var FormField = function FormField(props) {
+    var type = props.type;
+
+    var Field = void 0;
+
+    if (type === 'textarea') Field = TextareaField;else if (type === 'group.checkbox') Field = CheckboxGroupField;else if (type === 'group.radio') Field = RadioGroupField;else if (type === 'group.select') Field = SelectGroupField;else Field = InputField;
+
+    return _react2.default.createElement(Field, props);
+};
+
+exports.default = FormField;
+
